e01ae6c3a1943a20bd368ea5f7045cf6ac027245
sanskritiiii <sanskrutikurvey@gmail.com>
2024-06-14 09:22:59 -0400

    Adding temporary changes to Github

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,20 +1,14 @@
-# Uncomment the required imports before adding the code
-
-# from django.shortcuts import render
-# from django.http import HttpResponseRedirect, HttpResponse
-# from django.contrib.auth.models import User
-# from django.shortcuts import get_object_or_404, render, redirect
-# from django.contrib.auth import logout
-# from django.contrib import messages
-# from datetime import datetime
-
-from django.http import JsonResponse
-from django.contrib.auth import login, authenticate
+from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404, render, redirect
+# from .models import related models
+# from .restapis import related methods
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+from datetime import datetime
 import logging
 import json
-from django.views.decorators.csrf import csrf_exempt
-# from .populate import initiate
-
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
@@ -23,43 +17,47 @@ logger = logging.getLogger(__name__)
 # Create your views here.
 
 # Create a `login_request` view to handle sign in request
-@csrf_exempt
-def login_user(request):
-    # Get username and password from request.POST dictionary
-    data = json.loads(request.body)
-    username = data['userName']
-    password = data['password']
-    # Try to check if provide credential can be authenticated
-    user = authenticate(username=username, password=password)
-    data = {"userName": username}
-    if user is not None:
-        # If user is valid, call login method to login current user
-        login(request, user)
-        data = {"userName": username, "status": "Authenticated"}
-    return JsonResponse(data)
+def login_request(request):
+    context = {}
+    # handle post request
+    if request.method == "POST":
+        # get use name and password from post request
+        username = request.POST['username']
+        password = request.POST['psw']
+
+        # authenticate user
+        user = authenticate(username=username, password=password)
+        # if authorized log in and redirect else do not login
+        if user is not None:
+            login(request, user)
+            return redirect("djangoapp:index")
+        else:
+            return redirect("djangoapp/index.html")
+    else:
+        return redirect("djangoapp/index.html")
 
 # Create a `logout_request` view to handle sign out request
-# def logout_request(request):
+#def logout_request(request):
 # ...
 
 # Create a `registration` view to handle sign up request
-# @csrf_exempt
-# def registration(request):
+#@csrf_exempt
+ #def registration(request):
 # ...
 
 # # Update the `get_dealerships` view to render the index page with
 # a list of dealerships
-# def get_dealerships(request):
+ #def get_dealerships(request):
 # ...
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
-# def get_dealer_reviews(request,dealer_id):
+ #def get_dealer_reviews(request,dealer_id):
 # ...
 
 # Create a `get_dealer_details` view to render the dealer details
-# def get_dealer_details(request, dealer_id):
+ #def get_dealer_details(request, dealer_id):
 # ...
 
 # Create a `add_review` view to submit a review
-# def add_review(request):
+ #def add_review(request):
 # ...
